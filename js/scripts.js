9160ca8751851b77cf33d1dfa399e42f688359d3
jacksonlevine <jack@netbug.us>
2023-01-19 11:44:17 -0800

    Co-authored-by: MGantz22 <MGantz22@users.noreply.github.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+window.onload = initialize
+
+
+function initialize() {
+  let form = document.querySelector("form")
+  form.onsubmit = takeForm
+}
+
+
+function takeForm(event) {
+  
+}
+function sdfsdf 
